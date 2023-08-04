261a90272cf4b4e55dffcfbb4a85776a4fde89a4
Slavska <slavskaya.anastasiia.o@gmail.com>
2023-08-05 02:00:33 +0300

    hw-3

--- models/contacts.js
+++ models/contacts.js
@@ -1,63 +1,27 @@
-import fs from "fs/promises";
-import path from "path";
-import { nanoid } from "nanoid";
+import { Schema, model } from "mongoose";
+import { handleSaveError, runValidateAtUpdate } from "./hooks.js";
 
-const contactsPath = path.resolve("models", "contacts.json");
+const contactSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+contactSchema.pre("findOneAndUpdate", runValidateAtUpdate);
 
-export const listContacts = async () => {
-  const data = await fs.readFile(contactsPath, "utf8");
-  return JSON.parse(data);
-};
+contactSchema.post("save", handleSaveError);
+contactSchema.post("findOneAndUpdate", handleSaveError);
 
-export const getContactById = async (contactId) => {
-  const contacts = await listContacts();
-  const result = contacts.find((item) => item.id === contactId);
-  return result || null;
-};
+const Contact = model("contact", contactSchema);
 
-export const removeContact = async (contactId) => {
-  const contacts = await listContacts();
-  const index = contacts.findIndex((item) => item.id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  const [result] = contacts.splice(index, 1);
-  await updateContactStorage(contacts);
-  return result;
-};
-
-export const addContact = async ({ name, email, phone }) => {
-  const contacts = await listContacts();
-  const newContact = {
-    id: nanoid(),
-    name,
-    email,
-    phone,
-  };
-  contacts.push(newContact);
-  await updateContactStorage(contacts);
-  return newContact;
-};
-
-const updateContactStorage = async (contacts) => {
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
-};
-
-export const updateContact = async (contactId, { name, email, phone }) => {
-  const data = await listContacts();
-  const index = data.findIndex((item) => item.id === contactId);
-  if (index === -1) {
-    return null;
-  }
-  data[index] = { contactId, name, email, phone };
-  await updateContactStorage(data);
-  return data[index];
-};
-
-export default {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
-};
+export default Contact;
